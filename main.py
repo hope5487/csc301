8181aa622c5aabb8dd900400ed77bcb09e1012cf
Sunghyun <roy.cho@mail.utoronto.ca>
2020-05-31 10:23:36 +0900

    Discount calculation update (+DB)

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+select1 = 10.0
+select2 = 20.0
+total = 33.9
+option = 0.0
+tax = 3.9
+dp = 0.0
+dc = 0.0
+
 
 class User(db.Model):
     __table_name__ = 'user'
@@ -20,19 +28,6 @@ class User(db.Model):
     def __init__(self, username, password):
         self.username = username
         self.password = password
-
-
-class Product(db.Model):
-    __table_name__ = 'product'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True)
-    price = db.Column(db.Float(80))
-    quantity = db.Column(db.Float(80))
-
-    def __init__(self, name, price):
-        self.name = name
-        self.price = price
-        self.quantity = 0
 
 
 class Coupon(db.Model):
@@ -47,23 +42,41 @@ class Coupon(db.Model):
 
 
 @app.route('/')
-@app.route('/<float:num1>%<float:num2>%<float:result>')
-def home(num1=10.0, num2=20.0, result=33.9):
-    return render_template('index.html', num1=num1, num2=num2, result=result)
+@app.route('/<float:num1>%<float:num2>%<float:shipping>%<float:vat>%<float:discount_percent>%'
+           '<float:result>%<float:discount_total>')
+def home(num1=10.0, num2=20.0, result=33.9, shipping=0.0, vat=3.9, discount_percent=dp, discount_total=dc):
+    print("home dc: " + str(discount_total))
+    return render_template('index.html', num1=num1, num2=num2, result=result,
+                           shipping=shipping, vat=vat, discount_percent=discount_percent, discount_total=discount_total)
 
 
 @app.route('/calculate', methods=['GET', 'POST'])
 def calculate():
+    global select1, select2, total, option, tax, dc, dp
     if request.method == 'POST':
         select1 = float(request.form.get('select1'))
-        print(select1)
         select2 = float(request.form.get('select2'))
-        print(select2)
-        option = float(request.form['shipping'])
-        result = round((select1 + select2 + option) * 1.13, 2)
-    else:
-        return None
-    return redirect(url_for('home', num1=select1, num2=select2, result=result))
+        option = float(request.form['radio-shipping'])
+    dt = (select1 + select2) * 0.01 * dp
+    dc = round(dt, 2)
+    print(dc)
+    price_before_tax = select1 + select2 + option - dt
+    tax = round(price_before_tax * 0.13, 2)
+    total = round(price_before_tax * 1.13, 2)
+    return redirect(url_for('home', num1=select1, num2=select2, result=total,
+                            shipping=option, vat=tax, discount_percent=dp, discount_total=dc))
+
+
+@app.route('/discount', methods=['POST'])
+def discount():
+    global dp
+    if request.method == 'POST':
+        discount_code = request.form['discount_code']
+        coupon = Coupon.query.filter_by(code=discount_code).first()
+        if coupon is not None:
+            dp = int(coupon.percent)
+            return redirect(url_for('calculate'))
+    return redirect(url_for('home'))
 
 
 @app.route('/checkout', methods=['POST'])
